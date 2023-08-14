8d8c312210c599b86a24647bf247205e42758d3f
vasileoleinic <vasilii.oleinic@indrivo.com>
2023-08-14 21:24:44 +0300

    Fix the issues with the startup of the application.

--- src/WebHooks/WebHooksRepository/Services/Infrastructure/WebHooksRepositoryInstaller.cs
+++ src/WebHooks/WebHooksRepository/Services/Infrastructure/WebHooksRepositoryInstaller.cs
@@ -1,4 +1,5 @@
 using Microsoft.Extensions.DependencyInjection;
+using WebHooks.WebHooksRepository.Contracts;
 
 namespace WebHooks.WebHooksRepository.Services.Infrastructure;
 
@@ -6,6 +7,7 @@ public static class WebHooksRepositoryInstaller
 {
     public static IServiceCollection AddWebHooksRepository(this IServiceCollection services)
     {
+        services.AddTransient<IWebHooksRepository, WebHooksRepository>();
         return services;
     }
 }
---
+++ src/WebHooks/WebHooksRepository/Services/WebHooksRepository.cs
@@ -0,0 +1,21 @@
+﻿using WebHooks.WebHooksRepository.Contracts;
+
+namespace WebHooks.WebHooksRepository.Services;
+
+public class WebHooksRepository : IWebHooksRepository
+{
+    public Task<bool> DeleteAsync(string id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<WebHook>> GetListAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> SaveAsync(WebHook webHook)
+    {
+        throw new NotImplementedException();
+    }
+}
