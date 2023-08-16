08e23a3536d09f2257bddb805f6c6ed4100e4138
Vasilii Oleinic <oleinicv@b2b.regn.net>
2023-08-16 11:59:09 +0300

    Added default docker file to the solution.

--- src/Notifications.Host.Web/Program.cs
+++ src/Notifications.Host.Web/Program.cs
@@ -1,45 +1,37 @@
-using ApplicationRegistry.Infrastructure;
-using EventProcessor.Infrastructure;
-using Push.Routing;
-using Push.Service.Infrastructure;
-using Shared.Messaging.IntegrationEvents;
-using Shared.Messaging.RabbitMQ;
-using WebHooks.WebHooksRepository.Services.Infrastructure;
-using WebHooks.WebHooksService.Routing;
-using WebHooks.WebHooksService.Services.Infrastructure;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Register modules
 
-builder.Services
-    .AddWebHooksService()
-    .AddWebHooksRepository()
-    .AddApplicationRegistryService()
-    .AddPushService()
-    .AddHostedService<WorkerService>()
-    .AddEventsProcessor()
-    .AddRabbitMQ(builder.Configuration);
+// builder.Services
+//     .AddWebHooksService()
+//     .AddWebHooksRepository()
+//     .AddApplicationRegistryService()
+//     .AddPushService()
+//     .AddHostedService<WorkerService>()
+//     .AddEventsProcessor()
+//     .AddRabbitMQ(builder.Configuration);
 
 var app = builder.Build();
 
 // Register available endpoints
-app
-    .UseWebHooksServiceEndpoints()
-    .UsePushServiceEndpoints();
+// app
+//     .UseWebHooksServiceEndpoints()
+//     .UsePushServiceEndpoints();
 
-app.MapGet("/", (IMessageSender messageSender) =>
-{
-    var message = new Message(
-        header: new Header("APPCODE", "SOME_TENANT"),
-        body: new EventReceived {Payload = "An integration event has been received"});
+app.MapGet("/", () => "Hello world");
 
-    var routingKey = RoutingKeys
-        .AppEventsTopic
-        .ReplaceAppCodePlaceholderWith("NOTIFICATIONS")
-        .ReplaceTenantCodePlaceholderWith("ONE");
-
-    messageSender.PublishMessage(message, routingKey);
-});
+// app.MapGet("/", (IMessageSender messageSender) =>
+// {
+//     var message = new Message(
+//         header: new Header("APPCODE", "SOME_TENANT"),
+//         body: new EventReceived {Payload = "An integration event has been received"});
+//
+//     var routingKey = RoutingKeys
+//         .AppEventsTopic
+//         .ReplaceAppCodePlaceholderWith("NOTIFICATIONS")
+//         .ReplaceTenantCodePlaceholderWith("ONE");
+//
+//     messageSender.PublishMessage(message, routingKey);
+// });
 
 app.Run();
