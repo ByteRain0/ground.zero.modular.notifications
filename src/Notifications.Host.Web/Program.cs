0ab555030362bf9faf0d58d0aca41f16da8a64de
Vasilii Oleinic <oleinicv@b2b.regn.net>
2023-08-16 12:11:08 +0300

    Added the final fix for api docker image

--- src/Notifications.Host.Web/Program.cs
+++ src/Notifications.Host.Web/Program.cs
@@ -1,37 +1,30 @@
+using ApplicationRegistry.Infrastructure;
+using EventProcessor.Infrastructure;
+using Push.Routing;
+using Push.Service.Infrastructure;
+using Shared.Messaging.RabbitMQ;
+using WebHooks.WebHooksRepository.Services.Infrastructure;
+using WebHooks.WebHooksService.Routing;
+using WebHooks.WebHooksService.Services.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Register modules
 
-// builder.Services
-//     .AddWebHooksService()
-//     .AddWebHooksRepository()
-//     .AddApplicationRegistryService()
-//     .AddPushService()
-//     .AddHostedService<WorkerService>()
-//     .AddEventsProcessor()
-//     .AddRabbitMQ(builder.Configuration);
+builder.Services
+    .AddWebHooksService()
+    .AddWebHooksRepository()
+    .AddApplicationRegistryService()
+    .AddPushService()
+    .AddHostedService<WorkerService>()
+    .AddEventsProcessor()
+    .AddRabbitMQ(builder.Configuration);
 
 var app = builder.Build();
 
 // Register available endpoints
-// app
-//     .UseWebHooksServiceEndpoints()
-//     .UsePushServiceEndpoints();
-
-app.MapGet("/", () => "Hello world");
-
-// app.MapGet("/", (IMessageSender messageSender) =>
-// {
-//     var message = new Message(
-//         header: new Header("APPCODE", "SOME_TENANT"),
-//         body: new EventReceived {Payload = "An integration event has been received"});
-//
-//     var routingKey = RoutingKeys
-//         .AppEventsTopic
-//         .ReplaceAppCodePlaceholderWith("NOTIFICATIONS")
-//         .ReplaceTenantCodePlaceholderWith("ONE");
-//
-//     messageSender.PublishMessage(message, routingKey);
-// });
+app
+    .UseWebHooksServiceEndpoints()
+    .UsePushServiceEndpoints();
 
 app.Run();
