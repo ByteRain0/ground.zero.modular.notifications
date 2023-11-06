ac364ec03b6e510a180ac0200b4d57ab53e062a4
Vasilii Oleinic <oleinicv@b2b.regn.net>
2023-11-06 16:26:14 +0200

    Added Serilog Logger that integrates with ELK stack.

---
+++ src/Shared/Logging/LoggingInstaller.cs
@@ -0,0 +1,17 @@
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Serilog;
+
+namespace Shared.Logging;
+
+public static class LoggingInstaller
+{
+    public static IServiceCollection AddSerilogLogging(this IServiceCollection services, IConfiguration configuration)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        return services;
+    }
+}
